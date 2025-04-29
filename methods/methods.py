0423edad0a575d9f90d8febf1c32ff997387e5e0
Norbert305 <sealsnorbert@yahoo.com>
2025-04-28 20:41:48 -0400

    methods

---
+++ methods/methods.py
@@ -0,0 +1,25 @@
+
+
+# Lambda function to convert a string to uppercase and lowercase
+
+lower_to_upper = lambda s: s.upper()
+upper_to_lower = lambda s: s.lower()
+
+print(lower_to_upper("hello world"))
+print(upper_to_lower("HELLO WORLD"))
+
+
+# String methods
+
+# str.upper() - Converts all characters in a string to uppercase
+red_to_RED = "red".upper()
+print(red_to_RED)
+
+# str.lower() - Converts all characters in a string to lowercase
+Green_to_green = "GREEN".lower()
+print(Green_to_green)
+
+# The replace() method replaces a specified phrase with another specified phrase.
+mom_replace = "I love my mom".replace("mom", "dad")
+print(mom_replace)
+
