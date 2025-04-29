1926ad36651044855534a94ee4698603d6f37fc6
Norbert305 <sealsnorbert@yahoo.com>
2025-04-28 20:15:51 -0400

    concatenate file added for refresher

---
+++ concatenate/concat.py
@@ -0,0 +1,11 @@
+
+
+
+first_name = "Bob"
+
+last_name = "Seals"
+
+print(first_name + last_name) # BobSeals ---> concatenation of strings
+print(first_name + " " + last_name) # Bob Seals ---> concatenation of strings with a space in between
+print(f"{first_name} {last_name}") # Bob Seals ---> f-string formatting
+print("My name is " + first_name + " " + last_name) # My name is Bob Seals ---> concatenation of strings with a space in between
