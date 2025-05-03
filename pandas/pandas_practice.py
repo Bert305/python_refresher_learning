d2f86d7c276e318f2c62d2cdd7b461e0f245b294
Norbert305 <sealsnorbert@yahoo.com>
2025-05-02 20:03:09 -0400

    more pandas

--- pandas/pandas_practice.py
+++ pandas/pandas_practice.py
@@ -3,7 +3,10 @@ import pandas as pd
 csv_file = '../sample_video_game_sales.csv'
 
 df = pd.read_csv(csv_file)
+
+new_df = df[['Genre', 'Year', 'Name']] # create a new dataframe with only the Genre, Year, and Name columns
 print(df) # print the entire dataframe
 print(df.head()) # by default, head() returns the first 5 rows of the dataframe
 print(df.head(10)) # returns the first 10 rows of the dataframe
+print(new_df) # print the new dataframe with only the Genre column
 
