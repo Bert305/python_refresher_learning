f3da4c28d5fa06d787afacbc6ecb1946e888437f
Norbert305 <sealsnorbert@yahoo.com>
2025-05-02 20:25:50 -0400

    files added

--- classes/encapsulation.py
+++ classes/encapsulation.py
@@ -13,8 +13,17 @@ class UserInfo:
            return True
        else:
            return False
+       
+   def check_email(self, email_to_check):
+       # if check_email field matches class constructor email field above then return True else return False
+       if email_to_check == self.email_address:
+           return True
+       else:
+           return False
 
 user = UserInfo('user123', 'abc@edf.ghi') # our instance 
 # apply user instance to check_username
 print(user.check_username('user123')) # returns True
 print(user.check_username('user456')) # returns False
+print(user.check_email('bert@gmail.com')) # returns False
+print(user.check_email('abc@edf.ghi')) # returns True
