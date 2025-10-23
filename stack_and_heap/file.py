b0494688a00f53c1debac9d988ca1e24b9be0454
Bert305 <norbert@miamiedtech.com>
2025-10-23 12:51:52 -0400

    more stack and heap with lifo and garbage collection inside heap

--- stack_and_heap/file.py
+++ stack_and_heap/file.py
@@ -67,24 +67,47 @@ print("Popped from stack (empty):", remove_from_stack())
 
 
 
-# Heap LIFO Example using heapq --> manages memory for objects, lists, dictionaries, class instances, etc.
+# Heap Example --> manages memory for objects, lists, dictionaries, class instances, etc.
+
+# Heap (no LIFO rule)
+
+# The heap doesn’t follow LIFO. Objects can live longer than the function that created them.
+
+def make_list():
+    a = [10, 20, 30]   # list stored in heap
+    return a           # reference returned
+
+def main():
+    lst1 = make_list()   # lst1 points to heap object
+    lst2 = make_list()   # lst2 points to another heap object
+    lst1.append(40)
+    print("lst1:", lst1) # lst1: [10, 20, 30, 40]
+    print("lst2:", lst2) # lst2: [10, 20, 30]
+
+main()
 
 
-import heapq
+# Heap flow
 
-# Create an empty heap
-heap = []
+# [10, 20, 30] is created in the heap twice (for lst1 and lst2).
 
-# Push items onto the heap
-heapq.heappush(heap, 5)
-heapq.heappush(heap, 1)
-heapq.heappush(heap, 3)
+# Even after make_list() ends, the lists remain in memory because lst1 and lst2 still reference them.
 
-print("Heap after pushes:", heap)
+# Garbage collector only frees them when nothing points to them anymore
 
-# Pop items from the heap (min comes out first)
-print("Popped:", heapq.heappop(heap))  # removes 1
-print("Popped:", heapq.heappop(heap))  # removes 3
-print("Popped:", heapq.heappop(heap))  # removes 5
 
-print("Heap after pops:", heap)
+
+# Garbage Collection example
+
+# When the data from the heap is no longer referenced, Python’s garbage collector frees that memory.
+
+def garbage_collection_example():
+    a = [1, 2, 3]
+    b = a
+    del a  # Remove reference to the list
+    # At this point, the list [1, 2, 3] is still referenced by b
+    print("returning no reference of a", b)
+    del b  # Remove last reference
+    # Now the list is no longer referenced and can be garbage collected
+    print("Both a and b references deleted, list is garbage collected freeing memory.")
+garbage_collection_example()
